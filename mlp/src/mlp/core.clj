d76986dd1ce5d99e9c102758b1a28621995cbe33
Leonardo Dalcin <leonardodalcin@outlook.com>
2018-05-10 14:29:42 -0300

    Plot test, filter functions.

--- mlp/src/mlp/core.clj
+++ mlp/src/mlp/core.clj
@@ -6,7 +6,9 @@
             [cheshire.core :refer :all]
             [clojure.java.jdbc :as jdbc]
             [clj-postgresql.core :as pg]
-            [java-jdbc.ddl :as ddl]))
+            [java-jdbc.ddl :as ddl]
+            [clj-time.core :as t]))
+(j/start-jutsu!)
 
 (def db (pg/pool :host "localhost" :port "5432" :user "postgres" :dbname "mlp" :password "masquesaco"))
 (try (jdbc/db-do-commands db
@@ -22,33 +24,36 @@
     [:splitcoefficient "character varying"]))
     (catch Exception e (print "Database already created\n")))
 
-(defn plot-points [graph-name data]
+(defn plot-points [graph-name x y]
+  (print x)
   (j/graph!
   graph-name
-  [{:x (first data)
-    :y (second data)
+  [{:x x
+    :y y
     :mode "markers"
     :type "scatter"}])
 )
-(defn save-recursive [market-data market-data-days] (
 
-    case (into [] market-data-days)
-      [] 0
-      (try
-        ((jdbc/insert! db :stocks
-          {
-           :date (name (first market-data-days))
-           :open (get-in market-data [(first market-data-days) (keyword "1. open")])
-           :high (get-in market-data [(first market-data-days) (keyword "2. high")])
-           :low (get-in market-data [(first market-data-days) (keyword "3. low")])
-           :close (get-in market-data [(first market-data-days) (keyword "4. close")])
-           :adjustedclose (get-in market-data [(first market-data-days) (keyword "5. adjusted close")])
-           :volume (get-in market-data [(first market-data-days) (keyword "6. volume")])
-           :dividendamount (get-in market-data [(first market-data-days) (keyword "7. dividend amount")])
-           :splitcoefficient (get-in market-data [(first market-data-days) (keyword "8. split coefficient")])
-          }
-        ) (save-recursive market-data (drop 1 market-data-days)))
-        (catch Exception e (save-recursive market-data (drop 1 market-data-days))))
+(defn get-all [] (into [] (jdbc/query db ["select date, high from stocks"])))
+
+(defn save-recursive [market-data market-data-days] (
+  case (into [] market-data-days)
+    [] 0
+    (try
+      ((jdbc/insert! db :stocks
+        {
+         :date (name (first market-data-days))
+         :open (get-in market-data [(first market-data-days) (keyword "1. open")])
+         :high (get-in market-data [(first market-data-days) (keyword "2. high")])
+         :low (get-in market-data [(first market-data-days) (keyword "3. low")])
+         :close (get-in market-data [(first market-data-days) (keyword "4. close")])
+         :adjustedclose (get-in market-data [(first market-data-days) (keyword "5. adjusted close")])
+         :volume (get-in market-data [(first market-data-days) (keyword "6. volume")])
+         :dividendamount (get-in market-data [(first market-data-days) (keyword "7. dividend amount")])
+         :splitcoefficient (get-in market-data [(first market-data-days) (keyword "8. split coefficient")])
+        }
+      ) (save-recursive market-data (drop 1 market-data-days)))
+      (catch Exception e (save-recursive market-data (drop 1 market-data-days))))
 ))
 
 (defn get-market-data [] (parse-string (:body (client/get "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo")), true))
@@ -56,4 +61,6 @@
   (def market-data (get (get-market-data) (keyword "Time Series (Daily)")))
   (def market-data-days (keys market-data))
   (save-recursive market-data market-data-days)
+  (def database-data (into [] (get-all)))
+  (plot-points "Test" (map (fn [obj] (get obj :date)) database-data) (map (fn [obj] (get obj :high)) database-data))
 )
